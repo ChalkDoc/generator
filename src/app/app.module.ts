0aca516a9deef6c8c3068780ddec0706b0f0250f
Michel Woldemedihin <mwoldemedihin@gmail.com>
2017-12-15 16:06:58 -0800

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { routing } from './app.routing';
 
 import { AppComponent } from './app.component';
 
- '@angular/platform-browser';
-import { HttpModule }      from '@angular/http';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, HttpModule, FormsModule
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
