449d3629c51fc26eed26add69a38ed8b758f1bf6
ReconScout77 <alexmurray93@hotmail.com>
2017-12-20 13:58:42 -0800

    add function to get the valid sets

--- src/app/services/genertor.service.ts
+++ src/app/services/genertor.service.ts
@@ -5,4 +5,22 @@ export class GenertorService {
 
   constructor() { }
 
+  //problemsToGenerate should be an int, arrayOfCombinations is assumed to be an array of arrays
+  /* solveForMin and solveForMax are the minimum and maximum allowed values for the variable to be solved for */
+  checkValues(problemsToGenerate, arrayOCombinations, solveForMin, solveForMax) {
+   	let validCombos = [];
+    while (arrayOfCombinations.length > 0 && validCombos.length < problemsToGenerate) {
+    	let i = Math.floor(Math.random() * arrayOfCombinations.length);
+
+      /* problemSolver should take in an array of values, use the library to solve for the last variable, and output the value of that variable */
+      let x = problemSolver(arrayOfCombinations[i]);
+
+      if(x >= solveForMin && x <= solveForMax) {
+      	arrayOfCombinations[i].push(x);
+        validCombos.push(arrayOfCombinations[i]);
+      }
+      arrayOfCombinations.splice(i, 1);
+    }
+    return validCombos;
+  }
 }
