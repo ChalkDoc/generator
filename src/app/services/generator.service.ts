665431e959a4654bc15f0695f75e199d29ea7f18
Guest User <Guest@epicodus-79.local>
2017-12-21 11:55:03 -0800

    take decimal places into account to generate sets.

--- src/app/services/generator.service.ts
+++ src/app/services/generator.service.ts
@@ -49,44 +49,78 @@ export class GeneratorService {
     return answerArray;
   }
 
+  /** This method inputs parameters of variables with min and max values and
+      generates sets that include all the permutations of these variables. **/
   generatePermutations(parametersArray: any[]) {
-    let temp = [];
-    let arrayOfSets = [];
-    let answerArray = [];
+    let temp = []; // Stores running array of values for each index place.
+    let answerArray = [];  // Array returned with all possible permutation sets.
+    // Locates index of last variable.
     let numberOfVariables = parametersArray.length-1;
 
+    /** For loop calculates the total number of permutations based on the range
+        of the input parameters.  **/
     var totalPermutations = 1;
     for (var i = 0; i < numberOfVariables; i++) {
-    	var range = parametersArray[i].max - parametersArray[i].min + 1;
+      /** Range includes subtracting minimum from maximum and adding 1 to
+        include the minimum and maximum numbers **/
+    	var range = (parametersArray[i].max - parametersArray[i].min) *
+      ((Math.pow(10, parametersArray[i].decPoint))) + 1;
     	totalPermutations *= range;
-      //console.log(totalPermutations);
+      console.log(totalPermutations);
     }
+    totalPermutations = 1000;
 
+    // Intializes temp[] array with all the minimum parameters.
     for (var i = 0; i < numberOfVariables; i++) {
     	temp[i] = parametersArray[i].min;
     }
 
+    /** This method is run as a for loop through all the calculated
+        Permutations.  The index value is only used in order to track when
+        all sets are generated. **/
     for (var index = 0; index < totalPermutations; index++) {
-    	var arrayValues = [];
+    	var arrayValues = [];  // This will be pushed to final answerArray.
 
+      // Sets arrayValues to previous temp[] values.
       for (var i = 0; i < numberOfVariables; i++) {
       	arrayValues[i] = temp[i];
       }
 
+      /** If statement is true if the last element has not reached the maximum
+          value.  It causes the last element to increase to the next in
+          order. **/
+          // Gets temp ready for the next run.
       if (temp[numberOfVariables - 1] <= parametersArray[numberOfVariables - 1].max) {
-        temp[numberOfVariables - 1]++;
-      }
-      else {
-        for (var i = numberOfVariables - 1; i >= 0; i--) {
-          if (temp[i] > parametersArray[i].max) {
-    				temp[i] = parametersArray[i].min;
-          	temp[i - 1]++;
-          }
-          arrayValues[i] = temp[i];
-        }
-        temp[numberOfVariables - 1]++;
+        temp[numberOfVariables - 1]
+        += 1/Math.pow(10, parametersArray[numberOfVariables - 1].decPoint);
+        //temp[numberOfVariables - 1] = temp[numberOfVariables - 1].toFixed(parametersArray[numberOfVariables - 1].decPoint);
       }
 
+      /** Else takes care of the situation where elements other than the last
+          one (right-most) need to be changed.  **/
+      else {
+        /** Index i starts on the right-most element and works to the left.
+            If this addition brings the temp[i-1] to above the maximum,
+            this will be taken care in the next iteration of the for loop.**/
+        for (var i = numberOfVariables - 1; i >= 0; i--) {
+          // if current temp[i] value has reached the parameter maximum.
+          if (temp[i] > parametersArray[i].max) {
+    				temp[i] = parametersArray[i].min; // current temp[i] set to minimum.
+            temp[i - 1]
+            += 1/Math.pow(10, parametersArray[i - 1].decPoint);
+
+            //temp[i - 1] = temp[i - 1].toFixed(parametersArray[i - 1].decPoint);
+            // temp[i-1] (one to the left) is added.
+          }
+          arrayValues[i] = temp[i];  /** For loop ends with that temp[i]
+          finalized in arrayValues. **/
+        }
+        // Adds to last element in array.
+        temp[numberOfVariables - 1]
+        += 1/Math.pow(10, parametersArray[numberOfVariables - 1].decPoint);
+        //temp[numberOfVariables - 1] = temp[numberOfVariables - 1].toFixed(parametersArray[numberOfVariables - 1].decPoint);
+      }
+      // For each index value, a set is pushed to the answerArray.
       answerArray.push(arrayValues);
     }
     return answerArray;
