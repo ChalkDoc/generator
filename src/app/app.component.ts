0a0ef99f1e071b48a4f636d54edd4ea5557a3e68
Guest User <Guest@epicodus-79.local>
2017-12-20 18:02:13 -0800

    random testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,13 @@ import 'rxjs/add/operator/map';
 import { FormsModule } from '@angular/forms';
 
 import { Variable } from './variable';
+import './../../node_modules/nerdamer/Solve.js';
+import './../../node_modules/nerdamer/Algebra.js';
+import './../../node_modules/nerdamer/Calculus.js';
+import './../../node_modules/nerdamer/Extra.js';
 
 declare var GuppyOSK: any;
+declare var nerdamer: any;
 
 @Component({
   selector: 'app-root',
@@ -34,6 +39,39 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     // let valuesArray = this.findValues('x^3 + x^2 + 5=-25', 'x', 2);
     // console.log(valuesArray);
+
+    let object = [];
+    let parametersArray = [
+      {name: 'a', min: 1, max: 5},
+      {name: 'b', min: 1, max: 5},
+      {name: 'c', min: 1, max: 5},
+      {name: 'x', min: 1, max: 5},
+      {name: 'y', min: 2, max: 7}
+    ];
+
+    let testSet = [1, 1, 1, 1, 1];
+
+    object.push('{');
+    for (let i = 0; i < parametersArray.length; i++) {
+      var fantastic = parametersArray[i].name + ': ' + '\'' + testSet[i] + '\'';
+      object.push(fantastic);
+    }
+      object.push('}')
+      var finalString = object.join("\"");
+      console.log(finalString);
+      console.log(object);
+      //object.push(parametersArray[i].name + ': ' + '\"' + testSet[i] + '\"');
+
+    var x = nerdamer('5*x+(x^2+2*x)*x+(x+2)');
+    console.log(x.toString());
+    var x = nerdamer('a + b + c + x + y', object)
+    //the substitutions was called but the functions weren't called
+    console.log(x.toString());
+    // var x = nerdamer('(x^2+2*x)*x+1+x+cos(y)',
+    //   for (var i = 0; i < parametersArray.length; i++) { object[i] }, ['expand', 'numer']);
+    // console.log(x.toString());
+    // var x = nerdamer('(x^2+2*x)*x+1+x+cos(y)', {y: '7'}, ['expand', 'numer']);
+    // console.log(x.text('decimals'));
   }
 
 }
