b7eb3b26826399abd05195025fb7a47ad655f1ef
Kim Schulze <kimberlykayschulze@gmail.com>
2018-01-09 16:32:40 -0800

    add logic to create a branch to calculate for a decimal answer.

--- src/app/services/generator.service.spec.ts
+++ src/app/services/generator.service.spec.ts
@@ -462,4 +462,56 @@ debugger;
     expect(result[1][2].length).toBe(1);
   });
 
+  it('should generate a set of values for a variable based on min, max, and decimal numbers', () => {
+    const variable = new Variable('a', 1, 1, 2);
+
+
+    const expectedResult = ['1.0', '1.1', '1.2', '1.3', '1.4', '1.5', '1.6', '1.7', '1.8', '1.9', '2.0'];
+    //const expectedResult = [1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0];
+
+    let result = _generatorService.generateRangeOfValues(variable);
+    
+    expect(result[0]).toBe(expectedResult[0]);
+    expect(result[1]).toBe(expectedResult[1]);
+    expect(result[2]).toBe(expectedResult[2]);
+    expect(result[3]).toBe(expectedResult[3]);
+    expect(result[4]).toBe(expectedResult[4]);
+    expect(result[5]).toBe(expectedResult[5]);
+    expect(result[6]).toBe(expectedResult[6]);
+    expect(result[7]).toBe(expectedResult[7]);
+    expect(result[8]).toBe(expectedResult[8]);
+    expect(result[9]).toBe(expectedResult[9]);
+  });
+
+  it('should choose a set of values based on the generated list of values', () => {
+    let valueList = [];
+    valueList[0] = [1.1, 1.2, 1.3, 1.4, 1.5];
+    valueList[1] = [2.1, 2.2, 2.3, 2.4, 2.5];
+    valueList[2] = [3.1, 3.2, 3.3, 3.4, 3.5];
+
+    let testSetResult = _generatorService.createTestSet(valueList);
+    
+    expect(valueList[0]).toContain(testSetResult[0]);
+    expect(valueList[1]).toContain(testSetResult[1]);
+    expect(valueList[2]).toContain(testSetResult[2]);
+  });
+
+  it('should generate a list of valid permutation sets given decimal parameters', () => {
+    const a = new Variable('a', 1, 1, 5);
+    const b = new Variable('b', 1, 1, 5);
+    const c = new Variable('c', 1, 1, 5);
+
+    const variables: Variable[] = [a, b, c];
+    const numberOfProblems = 5;
+
+    const equation = 'a^2 + b^2 = c^2';
+    const expression = _generatorService.simplifyEquation(equation, variables[variables.length - 1].name);
+
+    let results = _generatorService.generateDecimalVariablesPermutations(variables, numberOfProblems, equation);
+
+    let expectedResult = _generatorService.solveForVariable(results[0], expression, variables);
+
+    expect(results[0][2]).toEqual(expectedResult[0]);
+  });
+
 });
