2577788e2f78ccecacaabae8c3510508efadd1c6
Mitch <meechllada@gmail.com>
2018-02-05 13:04:47 -0800

    add specs that test isVariableInArray method for decimal variables and integer variables

--- src/app/utilities/utilities.spec.ts
+++ src/app/utilities/utilities.spec.ts
@@ -14,11 +14,13 @@ import {
   calculateDecimalPlaces,
   pullRandomValue,
   genRandomPermutation,
-  getRangeValues
+  getRangeValues,
+  isVariableInArray
 } from './utilities';
 import * as _ from 'lodash';
 import { Variable } from '../variable';
 import { randomBytes } from 'crypto';
+import { checkAndUpdateBinding } from '@angular/core/src/view/util';
 
 describe('Utilities methods', () => {
   const varObjA = new Variable('a', 0, _.random(0, 5, false), _.random(15, 20, false));
@@ -40,20 +42,20 @@ describe('Utilities methods', () => {
       expect(knownVarsObj).toEqual({[varObjA.name]: String(randomSet[0]), [varObjB.name]: String(randomSet[1]), [varObjC.name]: String(randomSet[2])});
     });
 
-    // describe('solveForUnknownVariable ', () => {
-    //   it('should take a set of variables and reurn an equation in terms of the last variable in the set', () => {
-    //     const simplifiedEquation = simplifyEquation(equation, 'x');
-    //     const solvedForUnknownVariable = solveForUnknownVariable(randomSet, simplifiedEquation, variables);
+    describe('solveForUnknownVariable ', () => {
+      it('should take a set of numbers, plug them into the simplified equation and return a number for the variable thats being solved for)', () => {
+        const simplifiedEquation = simplifyEquation(equation, 'x');
+        const solvedForUnknownVariable = solveForUnknownVariable(randomSet, simplifiedEquation, variables);
 
-    //     expect(solvedForUnknownVariable).toEqual([ String(randomSet[0] * randomSet[1] + randomSet[2]]));
-    // });
+        expect(solvedForUnknownVariable).toEqual(randomSet[0] * randomSet[1] + randomSet[2]);
+    });
 
-    // describe('simplifyEquation', () => {
-    //   it('should return the equation in terms of variable being solved for', () => {
-    //     const simplifiedEquation = simplifyEquation(equation, 'b');
-    //     expect(simplifiedEquation).toEqual([String(randomSet[0] * randomSet[1] + randomSet[2]]));
-    //   });
-    // });
+    describe('simplifyEquation', () => {
+      it('should return the equation in terms of variable being solved for', () => {
+        const simplifiedEquation = simplifyEquation(equation, 'b');
+        expect(simplifiedEquation).toEqual('[-(-x+c)*a^(-1)]');
+      });
+    });
 
     describe('calculateDecimalPlaces', () => {
       it('checks the value returned by calculateDecimalPlaces when a string is given as parameter', () => {
@@ -102,8 +104,55 @@ describe('Utilities methods', () => {
 
         });
       });
+
+      describe('isVariableInArray method', () => {
+
+        describe('isVariableInArray for arrays of integer values', () => {
+          it('should check isVariableInArray when variable is not in array', () => {
+            const checkVariableIsInArray = isVariableInArray([3, 4, 5], [[3, 5, 6], [2, 3, 4]]);
+            expect(checkVariableIsInArray).toEqual(false);
+          });
+          it('should check isVariableInArray when variable is in array', () => {
+            const checkVariableIsInArray = isVariableInArray([3, 4, 5], [[3, 4, 5], [2, 3, 4]]);
+            expect(checkVariableIsInArray).toBe(true);
+          });
+        });
+
+        describe('isVariableInArray for arrays of decimal values', () => {
+          it('isVariableInArray for cases when variable is not in the array', () => {
+            const checkVariableIsInArray = isVariableInArray([3.4, 4.2, 5.9], [[3.4, 5.8, 6.8], [2.4, 3.6, 4.9]]);
+            expect(checkVariableIsInArray).toEqual(false);
+          });
+          it('should check isVariableInArray for cases when variable is in the array', () => {
+            const checkVariableIsInArray = isVariableInArray([3.4, 4.2, 5.9], [[3.4, 5.8, 6.8], [3.4, 4.2, 5.9]]);
+            expect(checkVariableIsInArray).toBe(true);
+          });
+        })
+      });
+
+
+      describe('getRandomValue', () => {
+        it('', )
+      })
+
+
+
     });
   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //   describe('The containsImaginary utility method', () => {
 //     it('should return false if input contains an "i" for imaginary', () => {
