316f57c0a2a991b0978240c0c3de93c56c0f9e66
Elrey Belmonti <elbelmonti@gmail.com>
2018-02-06 18:27:43 -0800

    WIP

--- src/app/guppy-input/guppy-input.component.ts
+++ src/app/guppy-input/guppy-input.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, ViewEncapsulation } from "@angular/core";
-import { NgForm } from "@angular/forms";
-import { Variable } from "../variable";
-import * as nerdamer from "nerdamer";
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Variable } from '../variable';
+import * as nerdamer from 'nerdamer';
+import * as _ from 'lodash';
 
 // import 'nerdamer/Solve';
 // import 'nerdamer/Algebra';
@@ -11,30 +12,34 @@ import * as nerdamer from "nerdamer";
 declare var Guppy: any;
 
 @Component({
-  selector: "guppy-input",
-  templateUrl: "./guppy-input.component.html",
-  styleUrls: ["./guppy-input.component.css"]
+  selector: 'guppy-input',
+  templateUrl: './guppy-input.component.html',
+  styleUrls: ['./guppy-input.component.css']
 })
 export class GuppyInputComponent implements OnInit {
   error: string;
   guppyParameterDiv = false;
   guppyVariables: Variable[] = [];
   guppyEquation: string;
+  equations: string[];
 
   constructor() {}
 
   ngOnInit() {
-    Guppy.init_symbols(["/assets/symbols.json"]);
-    const guppy = new Guppy("equationBox");
+    this.equations = [];
+    Guppy.init_symbols(['/assets/symbols.json']);
+    this.addGuppyBox();
+
+    // const guppy2 = new Guppy('equationBox2');
   }
 
   /* this is to get the content of the guppy box,
     it also converts the mathematical equation/expression into the desired format type(latex, asciimath, text, ast, eqns, function, xml)*/
   output() {
     try {
-      this.error = "";
-      this.guppyEquation = Guppy.instances["equationBox"].backend.get_content(
-        "text"
+      this.error = '';
+      this.guppyEquation = Guppy.instances['equationBox2'].backend.get_content(
+        'text'
       );
       const extractedVars = nerdamer(this.guppyEquation).variables();
       /* creating variable instance and pushing each variable instance into the variables array*/
@@ -43,10 +48,15 @@ export class GuppyInputComponent implements OnInit {
         const newVar = new Variable(varName);
         this.guppyVariables.push(newVar);
       }
-      console.log(this.guppyVariables, "guppy");
+      console.log(this.guppyVariables, 'guppy');
       this.guppyParameterDiv = true;
     } catch (e) {
-      this.error = "Parsing error!" + e;
+      this.error = 'Parsing error!' + e;
     }
+  }
+
+  addGuppyBox() {
+    const guppy = new Guppy(_.uniqueId('equationBox_'));
+    this.equations.push(guppy);
   }
 }
